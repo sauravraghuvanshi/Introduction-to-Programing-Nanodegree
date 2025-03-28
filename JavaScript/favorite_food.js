9b090e53e01e47e82a7becd205bf98f489e195ba
sauravraghuvanshi <sauravraghuvanshi24@gmail.com>
2025-03-29 00:33:48 +0530

    Add favorite food quiz with requirements and console.log example in favorite_food.js

---
+++ JavaScript/favorite_food.js
@@ -0,0 +1,11 @@
+/*
+ * Programming Quiz: Favorite Food
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * 1. Your code should print a string with your favorite food
+ * 2. Your code should have the first character capitalized
+ * 3. Your code must have a `console.log()` function
+ */
+console.log("Masala Paneer"/* replace this comment with your string */);
