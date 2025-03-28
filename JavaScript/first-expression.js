370904255abffbb5c33755b853270412fb5530f0
sauravraghuvanshi <sauravraghuvanshi24@gmail.com>
2025-03-28 22:22:49 +0530

    Add quiz requirements and initial expression example in first-expression.js

---
+++ JavaScript/first-expression.js
@@ -0,0 +1,10 @@
+/*
+ * QUIZ REQUIREMENTS
+ * 1. Your code should print the value `42`
+ * 2. You should use at least 3 distinct operators.
+ *    (`+`, `-`, `*`, `/`, or `%`) 
+ */
+
+
+// this expression equals 4, change it to equal 42
+console.log(39 + 5 - 2);
