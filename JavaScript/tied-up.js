040b2d327dd6c38ae850633de8b6d3ff41721dc2
sauravraghuvanshi <sauravraghuvanshi24@gmail.com>
2025-03-29 11:18:29 +0530

    Add programming quizzes for "All Tied Up" and "Yosa Buson" with requirements and examples

---
+++ JavaScript/tied-up.js
@@ -0,0 +1,10 @@
+/*
+ * Programming Quiz: All Tied Up
+ 
+ * Quiz Requirements
+ * 1. Your code should have a variable joke
+ * 2. Your joke should use only one string
+ * 3. Your joke should match the expected format
+*/
+const joke = "Why couldn\'t the shoes go out and play?\nThey were all \"tied\" up\!"
+console.log(joke);
---
+++ JavaScript/yosa-buson.js
@@ -0,0 +1,13 @@
+/*
+ * Programming Quiz: Yosa Buson
+ */
+ 
+/*
+ * QUIZ REQUIREMENTS
+ * 1. Your code should have a variable `haiku`
+ * 2. Your code should use string concatenation
+ * 3. Your poem should match the famous haiku poem
+ */
+
+const haiku = "Blowing from the west\n" + "Fallen leaves gather\n" + "In the east."
+console.log(haiku);
